a0810b326a251850e3dc4fcf8b8d76bd9ffe3a55
Aakash Tyagi <aakashtyagi354@gmail.com>
2021-10-08 10:48:11 +0530

    linked list file added

---
+++ traversing.java
@@ -0,0 +1,10 @@
+
+public class traversing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("hello world");
+         for(int i =0)
+	}
+
+}
